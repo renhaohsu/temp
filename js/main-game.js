a912e77ca20bb335c7201caa53dba1086fc59521
Your Name <zesterisk@gmail.com>
2018-04-21 16:52:09 +0800

    add an NPC
    but it would have some little troubles
     if i want to add something more in the future

--- js/main-game.js
+++ js/main-game.js
@@ -29,12 +29,33 @@ var ballRed = random(0,255);
 var ballGreen = random(0,255);
 var ballBlue = random(0,255);
 
-// paddle
+// paddle  現在是玩家角色
 var paddleHeight = 65;
 var paddleWidth = 10;
-var paddleX = (canvas.width-paddleWidth)/2
-var paddleVelX = 7;
-// var paddleVelY = -2;
+var paddleX = (canvas.width-paddleWidth)/10;
+var paddleVelX = 5;
+
+// NPC
+var NPCHeight = 65;
+var NPCWidth = 10;
+var NPCX = 9 * (canvas.width-NPCWidth)/10;  // 之後試試改成random(10,(canvas.width-10))
+var NPCVelX = 7;
+// var NPCisdefeated = false;  //突然用不到了
+// NPC's 血條
+var NPC_hp_Height = 10;
+var NPC_hp_Width = 50;
+var NPC_hp_X = NPCX - (0.45 * NPC_hp_Width);
+var NPC_hp_Y = canvas.height - NPCHeight - NPC_hp_Height - 10;
+
+
+// point
+var point = false;
+var pointHeight = 10;
+var pointWidth = 10;
+var pointX = NPCX;
+var pointY = canvas.height - NPCHeight - pointHeight - 10;
+
+
 
 // magic attact water
 var waterHeight = 45;
@@ -60,6 +81,27 @@ var spacePressed = false;
 // }
 
 
+
+function drawNPC() {
+	ctx.beginPath();
+	ctx.rect(NPCX, canvas.height-NPCHeight, NPCWidth, NPCHeight);
+	ctx.fillStyle = "rgba(255,0,100,0.8)"
+	ctx.fill();
+	ctx.closePath();
+}
+
+
+
+function drawNPC_hp() {
+	ctx.beginPath();
+	ctx.rect(NPC_hp_X , NPC_hp_Y, NPC_hp_Width, NPC_hp_Height);
+	ctx.fillStyle = "rgba(100,255,100,0.8)"
+	ctx.fill();
+	ctx.closePath();
+}
+
+
+
 function drawPaddle() {
 	ctx.beginPath();
 	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -67,6 +109,7 @@ function drawPaddle() {
 	ctx.fill();
 	ctx.closePath();
 }
+
 
 
 function drawWater() {
@@ -77,11 +120,62 @@ function drawWater() {
 	ctx.closePath();
 }
 
+function drawPoint() {
+	ctx.beginPath();
+	ctx.rect(pointX, pointY, pointWidth, pointHeight);
+	ctx.fillStyle = "rgba(0,0,100,0.8)"
+	ctx.fill();
+	ctx.closePath();
+}
+
+
+
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // drawBall();
     drawPaddle();
+
+    // 畫出NPC
+    if (NPC_hp_Width){
+    	drawNPC();
+    	drawNPC_hp();
+		if(spacePressed && NPCX > waterX && NPCX < waterX+waterWidth) {
+			// console.log(NPCisBeingHitted);
+			// NPCisBeingHitted = true;
+			if (NPC_hp_Width > 0) {
+				NPC_hp_Width --;
+			} else if (NPC_hp_Width <= 0){
+				NPC_hp_Width = 0;
+				NPCisdefeated = true;
+			}
+		}
+	// } else {
+	// 	console.log(NPCisBeingHitted);
+	// 	NPCisBeingHitted = false;
+	// }
+	} else if (!NPC_hp_Width){
+		// 讓npc在同一個地方被畫出來看起來會像解決了(若NPCX給random則會因為setinteval而位置更新300次)
+		// 這會造成在第一隻NPC消失時+1但之後都不會再加NPC也不會再出現
+		// 暫時還沒想到怎麼改 再用個變數來存NPC存活狀態?
+		// if (!point){
+		// 	drawPoint();
+		// 	point ++;
+		// 	setTimeout(()=>{NPC_hp_Width=50; NPCX=random(10,canvas.width-10);NPC_hp_X = NPCX - (0.45 * NPC_hp_Width);}, 3000);
+
+		// } else if (point){
+		// 	console.log(point);
+		// }
+			setTimeout(()=>{
+				NPC_hp_Width=50;
+				// NPCX=random(10,(canvas.width-10));
+				// NPC_hp_X = NPCX - (0.45 * NPC_hp_Width);
+			}, 3000);
+			// NPCisdefeated = false;
+		
+
+	}
+
 
     if(x + dx > canvas.width-ballRadius || x + dx < 0) {
     	dx = -dx;
@@ -104,11 +198,13 @@ function draw() {
 
 
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    	paddleVelX = 7
+    	paddleVelX = 5;
     	paddleX += paddleVelX;
+    	// waterX = paddleX;
 	} else if(leftPressed && paddleX > 0) {
-		paddleVelX = -7
+		paddleVelX = -5
     	paddleX += paddleVelX;
+    	// waterX = paddleX;
 	}
 
 	if(spacePressed) {
