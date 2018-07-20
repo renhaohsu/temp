e1b231d0dd4c9c2d339e8f189204d6da301c014e
zesterisk <34151904+zesterisk@users.noreply.github.com>
2018-07-20 11:05:32 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,127 +1,42 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-
-var width = window.innerWidth;
-var height = window.innerHeight;
-
-// canvas.width = width - 2.01;
-// canvas.height = height - 2.01;
-// canvas 右邊和下方會超出範圍造成捲軸出現
-// 但我完全想不到這個2是怎樣出來的 (而且上方左方沒有--線索不夠阿@@
-
-canvas.width = 0.5 * width ;
-canvas.height = 0.5 * height;
-
-// function to generate random number
-function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
-  return num;
+function setup() {
+  createCanvas(640, 360);
+  for (var i = 0; i < 1000; i++) {
+    drops[i] = new Drop();
+  }
 }
-
-
-// ball
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var ballRadius = 10;
-var ballRed = random(0,255);
-var ballGreen = random(0,255);
-var ballBlue = random(0,255);
-
-// paddle
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2
-
-// keyboard controls
-var rightPressed = false;
-var leftPressed = false;
-
-
-
-
-
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = `rgba(${ballRed},${ballGreen},${ballBlue},0.85)`;
-    ctx.fill();
-    ctx.closePath();
-}
-
-
-function drawPaddle() {
-	ctx.beginPath();
-	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-	ctx.fillStyle = "#0095DD"
-	ctx.fill();
-	ctx.closePath();
-}
-
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-
-    if(x + dx > canvas.width-ballRadius || x + dx < 0) {
-    	dx = -dx;
-
-		ballRed = random(0,255);
-		ballGreen = random(0,255);
-		ballBlue = random(0,255);
-	}
-
-	if(y + dy > canvas.height-ballRadius || y + dy < 0) {
-	    dy = -dy;
-
-		ballRed = random(0,255);
-		ballGreen = random(0,255);
-		ballBlue = random(0,255);
-	}
-
-    x += dx;
-    y += dy;
-
-
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    	paddleX += 7;
-	} else if(leftPressed && paddleX > 0) {
-    	paddleX -= 7;
-	}
-
-
+  background(0, 248, 1);
+  for (var i = 0; i < drops.length; i++) {
+    drops[i].fall();
+    drops[i].show();
+  }
 }
 
 
+function Drop() {
+  this.x = random(width);
+  this.y = random(-500, -50);
+  this.z = random(0, 20);
+  this.len = map(this.z, 0, 20, 10, 20);
+  this.yspeed = map(this.z, 0, 20, 1, 20);
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+  this.fall = function() {
+    this.y = this.y + this.yspeed;
+    var grav = map(this.z, 0, 20, 0, 0.15);
+    this.yspeed = this.yspeed + grav;
 
-function keyDownHandler(e) {
-	if(e.keyCode == 39) {
-		rightPressed = true;
-	} else if(e.keyCode == 37) {
-		leftPressed = true;
-	}
+    if (this.y > height) {
+      this.y = random(-200, -100);
+      this.yspeed = map(this.z, 0, 20, 4, 10);
+    }
+  }
+
+  this.show = function() {
+    var thick = map(this.z, 0, 20, 1, 4);
+    strokeWeight(thick);
+    stroke(255, 255, 255);
+    line(this.x, this.y, this.x, this.y+this.len);
+  }
 }
-
-function keyUpHandler(e) {
-	if(e.keyCode == 39) {
-		rightPressed = false;
-	} else if(e.keyCode=37){
-		leftPressed = false;
-	}
-}
-
-
-
-
-
-
-
-setInterval(draw, 10);
-
 
